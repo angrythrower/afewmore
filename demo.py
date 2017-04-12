51e224a8f255c8f78284ec801cddceb329cb1673
Tianxiao Yang <yangtianxiao123@gmail.com>
2017-04-12 14:04:22 -0400

    demoing execute function

--- demo.py
+++ demo.py
@@ -1,1 +1,36 @@
-print "hello world`"
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import json
+
+import os
+import signal
+import time
+
+def execute(cmd, timeout=None):
+
+    try:
+        pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True, preexec_fn=os.setsid) 
+        output, error = pro.communicate()
+
+        if timeout is not None:
+            time.sleep(timeout)
+            log("kill command: \n\t{0}".format(cmd))
+            os.killpg(pro.pid, signal.SIGTERM)
+            return (output, error)
+
+        if output:
+            # log("successfully executed command: \n\t{0}".format(cmd))
+            return (output, None)
+        if error:
+            log("error occurred when excuting command: \n\t{0}".format(cmd)) 
+            return (None, error)
+        return ("", None)
+
+    except OSError, oserror:
+        return (None, oserror)
+
+out, err = execute('ls')
+print out
