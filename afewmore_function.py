91e86c925298b05760f27c87db2086e7ac97418f
Tianxiao Yang <yangtianxiao123@gmail.com>
2017-04-12 14:19:54 -0400

    implementing execute function

--- afewmore_function.py
+++ afewmore_function.py
@@ -21,7 +21,28 @@ def elog(line):
     sys.exit(1)
 
 def execute(cmd, timeout=None):
-    pass
+
+    try:
+        pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True, preexec_fn=os.setsid) 
+        output, error = pro.communicate()
+
+        if timeout is not None:
+            time.sleep(timeout)
+            log("kill command: \n\t{0}".format(cmd))
+            os.killpg(pro.pid, signal.SIGTERM)
+            return (output, error)
+
+        if output:
+            # log("successfully executed command: \n\t{0}".format(cmd))
+            return (output, None)
+        if error:
+            log("error occurred when excuting command: \n\t{0}".format(cmd)) 
+            return (None, error)
+        return ("", None)
+
+    except OSError, oserror:
+        return (None, oserror)
 
 def host_parser(dir):
     pass
